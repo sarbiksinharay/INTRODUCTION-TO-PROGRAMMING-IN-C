e1c4e892b5507b3f18832f9755890cbc57576007
Sarbik Sinha Ray <sarbiksinharay@users.noreply.github.com>
2023-11-20 00:00:26 +0530

    Update fabonacci.c

--- fabonacci.c
+++ fabonacci.c
@@ -1,12 +1,13 @@
 #include<stdio.h>
 int main(){
-    int lines,temp1=1,temp2=1,backup=0;
+    int lines,temp1=1,temp2=0,backup=0;
     printf("ENTER THE NUMBER OF LINES:");
     scanf(" %d",&lines);
-    for(int i=1;i<=lines;i++){
-       backup=temp2;
-       temp2=temp2+temp1;
-       temp1=backup;
-        printf(" %d",temp2);
+    printf("%d\n",0);
+    for(int i=1;i<lines;i++){
+        backup=temp2;
+        temp2=temp2+temp1;
+        temp1=backup;
+        printf("%d\n",temp2);
     }
 }
