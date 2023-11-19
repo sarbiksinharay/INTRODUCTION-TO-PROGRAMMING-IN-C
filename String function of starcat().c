c92af3e19b8ed76128b2b5572c76e4d4054c4614
Sarbik Sinha Ray <sarbiksinharay@users.noreply.github.com>
2023-11-20 00:01:07 +0530

    Create String function of starcat().c

---
+++ String function of starcat().c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+#include<string.h>
+int main(){
+    char s1[]="ADAMAS UNIVERSITY";
+    char s2[]=" BCA SECTION H";
+    strcat(s1,s2);
+    printf("%s",s1);
+
+    return 0;
+}
